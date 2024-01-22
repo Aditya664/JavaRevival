54f709776c06b3f038c6f77eeb44319b98956411
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2024-01-22 19:35:30 +0530

    basic thing learned

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!!!");
+        int num1  = 33;
+        int num2 = 33;
+        System.out.println(num1 + num2);
+    }
+}
