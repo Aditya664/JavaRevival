2058b9b080c664362769ddf664e907a6c3639765
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2024-01-22 20:56:21 +0530

    completed all primitive datatypes

--- Hello.java
+++ Hello.java
@@ -1,3 +1,20 @@
+/*
+Primitive data types in Java are predefined by the Java language and named
+as the reserved keywords. A primitive data type does not share a state with
+other primitive values. Java programming language supports the following eight
+primitive data types.
+
+        Boolean data type
+        byte data type
+        int data type
+        long data type
+        float data type
+        double data type
+        char data type
+        short data type
+*/
+
+
 public class Hello {
     public static void main(String[] args) {
         // Primitive Datatypes => Integers
@@ -16,5 +33,14 @@ public class Hello {
         double doubleVariable = 222222.333333333333;
         System.out.println("Float variable " + floatVariable);
         System.out.println("Double variable " + doubleVariable);
+
+        // Primitive Datatypes => Boolean
+        boolean booleanVariable = true;
+        System.out.println("Boolean variable" + booleanVariable);
+
+        // Primitive Datatypes => Char
+        char charVariable = 'A';
+        System.out.println("Char variable" + charVariable);
+
     }
 }
