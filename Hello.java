b95025ebeb29d9fc57613f38ce04e8e732178de9
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2024-01-22 20:46:15 +0530

    Other primitive datatypes

--- Hello.java
+++ Hello.java
@@ -1,8 +1,20 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Java!!!");
-        int num1  = 33;
-        int num2 = 33;
-        System.out.println(num1 + num2);
+        // Primitive Datatypes => Integers
+        int intVariable = 33;
+        byte byteVariable = 122;
+        short shortVariable = 333;
+        long longVariable = 3333333333l;
+
+        System.out.println("Int variable " + intVariable);
+        System.out.println("Byte variable " + byteVariable);
+        System.out.println("Short variable " + shortVariable);
+        System.out.println("Long variable " + longVariable);
+
+        // Primitive Datatypes => Floating
+        float floatVariable = 123.2f;
+        double doubleVariable = 222222.333333333333;
+        System.out.println("Float variable " + floatVariable);
+        System.out.println("Double variable " + doubleVariable);
     }
 }
